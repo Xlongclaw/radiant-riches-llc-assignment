fa948332e98a5e46a3304077837018036c430e42
unknown <xlongclaw@gmail.com>
2024-02-25 19:51:59 +0530

    responsiveness added level_2

--- src/components/MainHighlight/MainHighLight.tsx
+++ src/components/MainHighlight/MainHighLight.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import styles from "./MainHighLight.module.scss";
 const MainHighLight = ({
   mainHighlight,
@@ -8,15 +10,34 @@ const MainHighLight = ({
   mainHighlightList: Array<{ rating: number; title: string }> | undefined;
   whyLove: Array<string> | undefined;
 }) => {
+  const [expanded, setExpanded] = useState(true);
+  useEffect(() => {
+    const resizeEvent = () => {
+      if (window.innerWidth < 1030) setExpanded(false);
+      else setExpanded(true);
+    };
+    window.addEventListener("resize", resizeEvent);
+    return () => {
+      window.removeEventListener("resize", resizeEvent);
+    };
+  }, []);
   return (
     <div className={styles.highlight}>
-      <h4 className={styles.heading}>Main highlights</h4>
-      {mainHighlight?.map((data, i) => (
-        <h4 key={`HIHGLIGHT_${i}`} className={styles.description}>
-          [{data.title}] : {data.description}
-        </h4>
-      ))}
-      {mainHighlightList && (
+      <h4 className={styles.heading}>
+        Main highlights{" "}
+        <img
+          onClick={() => setExpanded(!expanded)}
+          src="images/chevron-down.png"
+          alt=""
+        />
+      </h4>
+      {expanded &&
+        mainHighlight?.map((data, i) => (
+          <h4 key={`HIHGLIGHT_${i}`} className={styles.description}>
+            [{data.title}] : {data.description}
+          </h4>
+        ))}
+      {expanded && mainHighlightList && (
         <div className={styles.highlightList}>
           {mainHighlightList.map((data, i) => (
             <div key={`HIGHLIGHT_LIST_ITEM_${i}`} className={styles.listItem}>
@@ -26,7 +47,7 @@ const MainHighLight = ({
           ))}
         </div>
       )}
-      {whyLove && (
+      {expanded && whyLove && (
         <div className={styles.whyloveContainer}>
           <h4 className={styles.head}>Why we love it?</h4>
           {whyLove.map((data, i) => (
@@ -37,10 +58,12 @@ const MainHighLight = ({
           ))}
         </div>
       )}
-      <button className={styles.button}>
-        Show more
-        <img src="./images/chevron-down-blue.png" alt="" />
-      </button>
+      {expanded && (
+        <button className={styles.button}>
+          Show more
+          <img src="./images/chevron-down-blue.png" alt="" />
+        </button>
+      )}
     </div>
   );
 };
